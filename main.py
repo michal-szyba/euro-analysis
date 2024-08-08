f96126bbf9486c4c244c65b67f56e4a337188b73
michal-szyba <mszyba@onet.pl>
2024-08-08 15:16:25 +0200

    relation between yellow cards and tournament stage

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
+import mysql
 from db_connect import get_connection
 import queries
+import matplotlib.pyplot as plt
 
-mydb = get_connection()
-cursor = mydb.cursor()
-cursor.execute(queries.first_match_cards)
-print(cursor.fetchall())
+
+def fetch_results(query):
+    try:
+        mydb = get_connection()
+        cursor = mydb.cursor()
+        cursor.execute(query)
+
+        results = cursor.fetchall()
+
+        cursor.close()
+        mydb.close()
+        return results
+    except Exception as e:
+        print(e)
+
+
+query_results = fetch_results(queries.cards_by_match_category)
+results = [(match_category, float(cards_sums)) for match_category, cards_sums in query_results]
+match_order = ['I mecz grupowy',
+               'II mecz grupowy',
+               'III mecz grupowy',
+               '1/8 finału',
+               '1/4 finału',
+               '1/2 finału',
+               'finał']
+
+sorted_results = sorted(results, key=lambda x: match_order.index(x[0]), )
+match_types, cards_sums = zip(*sorted_results)
+
+plt.figure(figsize=(8, 8))
+plt.xticks(rotation=60)
+plt.subplots_adjust(bottom=0.2)
+plt.bar(match_types, cards_sums, width=0.6, align='center')
+plt.show()
--- queries.py
+++ queries.py
@@ -1,1 +1,12 @@
-first_match_cards = "SELECT match_category FROM csv_Meczyki_euro WHERE team_one = 'Niemcy' OR team_two = 'Niemcy'"
+first_match_cards = ("SELECT match_category "
+                     "FROM csv_Meczyki_euro "
+                     "WHERE team_one = 'Niemcy' OR team_two = 'Niemcy'")
+cards_group_stage = ("SELECT yellow_cards_sum "
+                     "FROM csv_Meczyki_euro "
+                     "WHERE match_category "
+                     "LIKE '% mecz grupowy'")
+cards_by_match_category = ("SELECT match_category, AVG(yellow_cards_sum) "
+                           "AS cards_sum "
+                           "FROM csv_Meczyki_euro "
+                           "GROUP BY match_category "
+                           "ORDER BY match_category")
