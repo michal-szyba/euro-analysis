88e6e5d6296817e9dc2f754c8affa35bf3c66290
michal-szyba <mszyba@onet.pl>
2024-08-12 17:47:46 +0200

    logistics fix number one

--- db_connect.py
+++ db_connect.py
@@ -4,10 +4,25 @@ import mysql.connector
 def get_connection():
     connection = mysql.connector.connect(
         host='localhost',
-        user='<USERNAME>',
+        user='<USER>',
         password='<PASSWORD>',
         database='eurostats'
     )
     if connection.is_connected():
         print('Connected succesfully')
         return connection
+
+
+def fetch_results(query):
+    try:
+        mydb = get_connection()
+        cursor = mydb.cursor()
+        cursor.execute(query)
+
+        results = cursor.fetchall()
+
+        cursor.close()
+        mydb.close()
+        return results
+    except Exception as e:
+        print(e)
