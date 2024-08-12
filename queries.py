45d823b2c7009cf356ba84fcacadec2d47e50e14
Szymon Pawłowski <szymon.pawlowski.social@gmail.com>
2024-08-12 17:49:52 +0200

    database name syntax error corrected

--- queries.py
+++ queries.py
@@ -1,17 +1,17 @@
 first_match_cards = ("SELECT match_category "
-                     "FROM csv_Meczyki_euro "
+                     "FROM csv_meczyki_euro "
                      "WHERE team_one = 'Niemcy' OR team_two = 'Niemcy'")
 cards_group_stage = ("SELECT yellow_cards_sum "
-                     "FROM csv_Meczyki_euro "
+                     "FROM csv_meczyki_euro "
                      "WHERE match_category "
                      "LIKE '% mecz grupowy'")
 cards_by_match_category = ("SELECT match_category, AVG(yellow_cards_sum) "
                            "AS cards_sum "
-                           "FROM csv_Meczyki_euro "
+                           "FROM csv_meczyki_euro "
                            "GROUP BY match_category "
                            "ORDER BY match_category")
 fouls_by_match_category = ("SELECT match_category, AVG(fouls_sum) "
                            "AS fouls_sum "
-                           "FROM csv_Meczyki_euro "
+                           "FROM csv_meczyki_euro "
                            "GROUP BY match_category "
                            "ORDER BY match_category ")
